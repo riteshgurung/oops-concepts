e5a4984af69ded0f0ae2aeb50c0db4876b4c35cc
Ritesh Gurung <ritesh@srijan.in>
2016-08-17 21:38:23 +0530

    Modified Directory structure

---
+++ modules/custom/acme/src/Controller/DefaultController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Drupal\acme\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountProxyInterface;
+
+class DefaultController extends ControllerBase {
+  public function hello($name) {
+    // the {name} in the route gets captured as $name variable
+    // in the function called
+    $this->userList();
+    return [
+      '#theme' => 'hello_page',
+      '#data' => array('name' => $name,'greetings' => $this->getGreetings()),
+      '#item' => array('href' => '/', 'caption' => $name . ' phone number'),
+      '#attached' => [
+        'library' => [
+          'acme/acme-styles', //include our custom library for this response
+        ]
+      ]
+    ];
+  }
+
+  private function getGreetings() {
+    $salutations = array(
+    "Adab",
+    "Ahoj",
+    "An-nyeong-ha-se-yo",
+    "Apa khabar",
+    "Barev Dzez",
+    "Buenos días",
+    "Bula Vinaka",
+    "Chào",
+    "Ciao",
+    "Dia Duit",
+    "Hallo",
+    "Hallå",
+    "Halló",
+    "Halo",
+    "Haye",
+    "Hei",
+    "Hej",
+    "Hello",
+    "Helo",
+    "Hola",
+    "Kamusta",
+    "Konnichiwa",
+    "Merhaba",
+    "Mingalarba",
+    "Namaskar",
+    "Namaste",
+    "Olá",
+    "Pryvit",
+    "Pryvitannie",
+    "Përshëndetje",
+    "Salam",
+    "Salut",
+    "Sat Sri Akal",
+    "Sholem aleikhem",
+    "Sveiki",
+    "Szia",
+    "Tere",
+    "Zdraveĭte",
+    "Zdravo" );
+    $salutation = $salutations[rand(0, count($salutations) - 1)];
+    return $salutation;
+  }
+
+  private function userList() {
+    $user_storage = \Drupal::entityManager()->getStorage('user');
+  }
+}
